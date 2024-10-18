b603035d59f831b255dc3e6695131856edf2cfc6
Ronald Bosma <ronaldb@infosupport.com>
2024-10-18 18:10:18 +0200

    Add method to get all policy expressions from a syntax tree

---
+++ src/APIM.Policies.CLI.Tests/Extensions/SyntaxTreeExtensionsTests.cs
@@ -0,0 +1,115 @@
+﻿using APIM.Policies.CLI.Extensions;
+using APIM.Policies.CLI.Models;
+using APIM.Policies.CLI.Tests.TestHelpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace APIM.Policies.CLI.Tests.Extensions;
+
+[TestClass]
+public class SyntaxTreeExtensionsTests
+{
+    [TestMethod]
+    public async Task GetPolicyExpressionsAsync_SourceHasSinglePolicyExpression_PolicyExpressionReturned()
+    {
+        //Arrange
+        var source = """
+            namespace A.Namespace;
+            internal class ClassName
+            {
+                public static bool MethodName(IPolicyContext context) => true;
+            }
+            """;
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var methods = await GetMethodsAsync(syntaxTree);
+        var model = SyntaxHelper.CreateSemanticModel(syntaxTree);
+
+        //Act
+        var result = await syntaxTree.GetPolicyExpressionsAsync(model);
+
+        //Assert
+        var expectedResult = new Dictionary<string, PolicyExpression>
+        {
+            { "A.Namespace.ClassName.MethodName", methods[0].ToPolicyExpression(model) }
+        };
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [TestMethod]
+    public async Task GetPolicyExpressionsAsync_SingleClassWithMultiplePolicyExpressions_PolicyExpressionsReturned()
+    {
+        //Arrange
+        var source = """
+            namespace A.Namespace;
+            internal class ClassName
+            {
+                public static bool MethodName1(IPolicyContext context) => true;
+                public static bool MethodName2(IPolicyContext context) => true;
+                public static bool MethodName3(IPolicyContext context) => true;
+            }
+            """;
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var methods = await GetMethodsAsync(syntaxTree);
+        var model = SyntaxHelper.CreateSemanticModel(syntaxTree);
+
+        //Act
+        var result = await syntaxTree.GetPolicyExpressionsAsync(model);
+
+        //Assert
+        var expectedResult = new Dictionary<string, PolicyExpression>
+        {
+            { "A.Namespace.ClassName.MethodName1", methods[0].ToPolicyExpression(model) },
+            { "A.Namespace.ClassName.MethodName2", methods[1].ToPolicyExpression(model) },
+            { "A.Namespace.ClassName.MethodName3", methods[2].ToPolicyExpression(model) }
+        };
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [TestMethod]
+    public async Task GetPolicyExpressionsAsync_PolicyExpressionsInDifferentClasses_PolicyExpressionsReturned()
+    {
+        //Arrange
+        var source = """
+            namespace A.Namespace;
+            internal class ClassName1
+            {
+                public static bool MethodName(IPolicyContext context) => true;
+            }
+            internal class ClassName2
+            {
+                public static bool MethodName(IPolicyContext context) => true;
+            }
+            internal class ClassName3
+            {
+                public static bool MethodName(IPolicyContext context) => true;
+            }
+            """;
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var methods = await GetMethodsAsync(syntaxTree);
+        var model = SyntaxHelper.CreateSemanticModel(syntaxTree);
+
+        //Act
+        var result = await syntaxTree.GetPolicyExpressionsAsync(model);
+
+        //Assert
+        var expectedResult = new Dictionary<string, PolicyExpression>
+        {
+            { "A.Namespace.ClassName1.MethodName", methods[0].ToPolicyExpression(model) },
+            { "A.Namespace.ClassName2.MethodName", methods[1].ToPolicyExpression(model) },
+            { "A.Namespace.ClassName3.MethodName", methods[2].ToPolicyExpression(model) }
+        };
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+
+    public static async Task<IList<MethodDeclarationSyntax>> GetMethodsAsync(SyntaxTree syntaxTree)
+    {
+        var root = await syntaxTree.GetRootAsync();
+
+        return root.DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
+    }
+}
---
+++ src/APIM.Policies.CLI/Extensions/SyntaxTreeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using APIM.Policies.CLI.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace APIM.Policies.CLI.Extensions;
+
+internal static class SyntaxTreeExtensions
+{
+    public static async Task<IDictionary<string, PolicyExpression>> GetPolicyExpressionsAsync(this SyntaxTree syntaxTree, SemanticModel model)
+    {
+        var root = await syntaxTree.GetRootAsync();
+        var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+        var policyExpressions = new Dictionary<string, PolicyExpression>();
+
+        foreach (var method in methods.Where(m => m.IsPolicyExpression(model)))
+        {
+            var policyExpression = method.ToPolicyExpression(model);
+            policyExpressions.Add(policyExpression.FullName, policyExpression);
+        }
+
+        return policyExpressions;
+    }
+}

4415863b8da37868813f271c8541130d1bf41098
Ronald Bosma <ronaldb@infosupport.com>
2024-10-18 17:57:42 +0200

    Add PolicyExpression record

---
+++ src/APIM.Policies.CLI/Models/PolicyExpression.cs
@@ -0,0 +1,8 @@
+﻿namespace APIM.Policies.CLI.Models;
+
+internal record PolicyExpression
+{
+    public required string FullName { get; init; }
+    public required string Body { get; init; }
+    public required bool IsSingleStatement { get; init; }
+}

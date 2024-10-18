fba58502ad9cd044d17d0982862a57bdfd8f599d
Ronald Bosma <ronaldb@infosupport.com>
2024-10-18 18:44:20 +0200

    Add expressions for GetEcho operation

---
+++ sample/APIM.Policies.Sample/EchoAPI/GetEcho/GetEchoExpressions.cs
@@ -0,0 +1,13 @@
+﻿namespace APIM.Policies.Sample.EchoAPI.PostEcho;
+
+internal class GetEchoExpressions
+{
+    //TODO: return context.Response.StatusCode >= 400
+    public static bool ResponseIsError(IPolicyContext context) => true;
+
+    public static string TransformResponse(IPolicyContext context)
+    {
+        string response = "This is the response";
+        return response;
+    }
+}

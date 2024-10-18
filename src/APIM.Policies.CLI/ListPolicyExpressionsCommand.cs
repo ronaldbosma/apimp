c784835329dc16c6d75e1171c86764295337df21
Ronald Bosma <ronaldb@infosupport.com>
2024-10-18 12:40:00 +0200

    List documents and methods

--- src/APIM.Policies.CLI/ListPolicyExpressionsCommand.cs
+++ src/APIM.Policies.CLI/ListPolicyExpressionsCommand.cs
@@ -1,16 +1,37 @@
 ﻿using APIM.Policies.Core;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
 using System.Reflection;
 
 namespace APIM.Policies.CLI
 {
-    internal sealed class ListPolicyExpressionsCommand : Command<ListPolicyExpressionsCommandSettings>
+    internal sealed class ListPolicyExpressionsCommand : AsyncCommand<ListPolicyExpressionsCommandSettings>
     {
-        public override int Execute(CommandContext context, ListPolicyExpressionsCommandSettings settings)
+
+        public override async Task<int> ExecuteAsync(CommandContext context, ListPolicyExpressionsCommandSettings settings)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(settings.Source, nameof(settings));
 
             AnsiConsole.MarkupLine("Load policy expressions from {0}", settings.Source);
 
+            MSBuildLocator.RegisterDefaults();
+            using var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(settings.Source);
+
+            foreach (var document in project.Documents)
+            {
+                AnsiConsole.MarkupLine(document.Name);
+
+                var syntaxTree = await document.GetSyntaxTreeAsync() ?? throw new Exception("Unable to load syntax tree");
+                var root = await syntaxTree.GetRootAsync();
+
+                var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+                foreach (var method in methods)
+                {
+                    AnsiConsole.MarkupLine($"- {method.Identifier.Text}");
+                }
+            }
 
             return 0;
         }
